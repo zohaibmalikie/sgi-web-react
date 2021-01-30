70e86b8ea00f58ad45e6a3274dd1e9556a88bb5e
Zohaib Malik <57034343+zohaibmalikie@users.noreply.github.com>
2021-01-30 19:05:47 +0500

    functionality added

--- src/component/calender/calender.jsx
+++ src/component/calender/calender.jsx
@@ -10,15 +10,15 @@ export default function Calender() {
 
     //   Last Month Days
     var prevDay = dt.getDay()-1;
-      console.log(prevDay);
+    //   console.log(prevDay);
      
      
      //  Current Month Days
-     var endDate = new Date(
-         dt.getFullYear(),
-         dt.getMonth()+1,
-         0
-         ).getDate();
+     var endDate = new Date(dt.getFullYear(), dt.getMonth()+1, 0 ).getDate();
+     var prev_Date = new Date(dt.getFullYear(),dt.getMonth(),0).getDate();
+    //  console.log(prev_Date)
+     var today =new Date().getDate();
+     console.log(today);
     
         
     
@@ -29,40 +29,54 @@ export default function Calender() {
 
 // Months Code
   let monthNumber = (new Date().getMonth());
-   monthNumber = monthNumber++;
+//    monthNumber = monthNumber++;
 let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let monthName = monthNames[monthNumber];
 
 // Days Code
 // Previous Month days
-var prev_Date = new Date(dt.getFullYear(),dt.getMonth(),0).getDate();
-console.log(prev_Date)
+
 
 let lastMonthDay = (endDate)=> {
     let monthDays = [];
-    for(let i=0; i<prevDay; i--){
+    for(let i=0; i<prevDay; i++){
        monthDays.push(prev_Date-i);
     }
-//    return monthDays.map((lastDay)=> {
-//         return (<div>{prev_Date}</div>) 
+   return monthDays.reverse().map((lastDay)=> {
+        return (<div className="prev-date">{lastDay}</div>) 
 
-//     })
+    })
 }
 // Current Month days
 let currentDays = (endDate)=> {
     let monthDays = [];
     for(let i=1; i<=endDate; i++){
+        
        monthDays.push(i);
     }
    return monthDays.map((singleDay)=> {
-        return (<div>{singleDay}</div>) 
-
+       if(singleDay==today.getDate && dt.getMonth()==today.getMonth()){
+           return (<div className="today">{singleDay}</div>) 
+       }else{
+           return (<div>{singleDay}</div>) 
+       }
     })
 }
 
 
+// Change Month on trigger
+const changeMonth=(value)=>{
+if (value=='prev'){
+   let monthNamePrev = monthNames[--monthNumber];
+   console.log(monthNamePrev);
+}else{
+    dt.setMonth(dt.getMonth()+1);
+    let monthNameNext = monthNames[++monthNumber]; 
+    console.log(monthNameNext);
+}
+}
 
-    return (
+return (
 <div className="wrapper container-fluid pt-4">
     <div className="calendar">
         <div className="month">
@@ -74,9 +88,9 @@ let currentDays = (endDate)=> {
             </div>
            
             <div className="triggers lightgray">
-                <span className="fa fa-chevron-left prev"></span>
+                <span className="fa fa-chevron-left prev" onClick={()=>changeMonth('prev')}></span>
                 &nbsp; &nbsp;
-                <span className="fa fa-chevron-right next"></span>
+                <span className="fa fa-chevron-right next" onClick={()=>changeMonth('next')}></span>
             </div>
         </div>
         <div className="weekend pt-3">
